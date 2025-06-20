dba68319064a0d16d990051381332068637e3f7d
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-06-20 17:32:54 -0500

    refactor: remove InteractiveCanvas tests, update dependencies, and integrate vite-tsconfig-paths plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
+    test: {
+        environment: "jsdom",
+        globals: true,
+    },
+    plugins: [tsconfigPaths()],
 });
